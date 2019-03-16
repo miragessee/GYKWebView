8226f906ca20a24156f8bd6ea395a5c10107dcb5
Hakan UNAL <hakanunal@hakansoftware.com>
2019-03-17 01:53:57 +0300

    v0

---
+++ app/src/main/java/com/example/hakan/gykwebview/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.hakan.gykwebview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+        });
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://gelecegiyazanlar.turkcell.com.tr");
+    }
+}
